8354a25d32fe9fd7a42d9711792373fb316e75c1
Haonan Chang <chnme40cs@gmail.com>
2024-01-24 16:35:06 -0500

    update: setup the running pipeline

--- README.md
+++ README.md
@@ -5,3 +5,23 @@ I am using KinectFusion for Reconstruction and BundleTrack for Object Tracking.
 ## Installation
 
 1. Compile BundleTrack. Install Dependency: `sudo apt install libpcl-dev`, `sudo apt-get install freeglut3-dev`
+
+## Run tracking
+
+### Generate 2D Tracking Mask
+
+1. First we need to get the mask first using `pytracking`. Download `RTS` checkpoint at https://github.com/visionml/pytracking/blob/master/MODEL_ZOO.md#Models-1. Notice that pytracking requires `torch` version to be lower than `1.9` and `numpy` should be lower that `1.20`.
+
+2. Run tracking. You can do the tracking using the following code:
+
+```python
+python run_video.py rts rts50 ../../../test_data/exp_0/camera_2/color/%d.jpg --save_results
+```
+
+### Generate 6D pose tracking
+
+```bash
+./build/bundle_track_colmap config.yml
+```
+
+## Experiment Setup
---
+++ generate_track_config.py
@@ -0,0 +1,31 @@
+"""Generate tracking config for BundleTracking"""
+import os
+import yaml
+import open3d as o3d
+
+
+if __name__ == "__main__":
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    asset_dir = os.path.join(root_dir, "assets")
+
+    bundletrack_path = os.path.join(root_dir, "external", "BundleTrack")
+    default_config = os.path.join(bundletrack_path, "config_colmap.yml")
+    # Read default config
+    with open(default_config, "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    # Update config
+    fix_camera_id = 2
+    config["data_dir"] = os.path.join(root_dir, "test_data", "exp_0", f"camera_{fix_camera_id}")
+    config["mask_dir"] = os.path.join(config["data_dir"], "seg")
+    config["model_dir"] = os.path.join(asset_dir, "unit_cube.obj")
+    config["debug_dir"] = os.path.join(root_dir, "log")
+    # Save config
+    config_file = os.path.join(root_dir, "config.yml")
+    with open(config_file, "w") as f:
+        yaml.dump(config, f)
+
+    # Save a unit cube obj
+    unit_cube_obj = o3d.geometry.TriangleMesh.create_box(1, 1, 1)
+    unit_cube_obj.compute_vertex_normals()
+    unit_cube_obj_file = os.path.join(asset_dir, "unit_cube.obj")
+    o3d.io.write_triangle_mesh(unit_cube_obj_file, unit_cube_obj)
